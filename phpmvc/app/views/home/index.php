a87f9452be40934121ad452c2b4e139aec741afd
Abeng6030 <116140161+Abeng6030@users.noreply.github.com>
2022-11-30 13:54:14 +0700

    Add files via upload
    OOP PHP

---
+++ phpmvc/app/views/home/index.php
@@ -0,0 +1,9 @@
+<div class="container">
+<div class="jumbotron mt-5">
+  <h1 class="display-4"><center>Welcome Users</center></h1>
+  <p class="lead"><center>Hello my name is <?=$data['nama'];?>.</center></p>
+  <hr class="my-4">
+  <p><center>Dan ini adalah Tugas Object Oriented Programming (OOP) Dari Pa Cahyadi.</center></p>
+  <!--a class="btn btn-primary btn-lg" href="https://www.php.net/docs.php" target="_blank" role="button">PHP Documentation</a-->
+</div>
+</div>
